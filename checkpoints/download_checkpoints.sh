9ef96555367b1c1b2dfdc15529a4816133f6fb37
ShiJianrui <s1075671577@gmail.com>
2024-11-09 01:43:56 +0800

    add script for downloading checkpoints

---
+++ checkpoints/download_checkpoints.sh
@@ -0,0 +1,19 @@
+curl 'https://drive.usercontent.google.com/download?id=1zTeGUfm3UL2loTtyb9toZqAnHoaTVCMW&export=download&authuser=0&confirm=t&uuid=e59ba739-224c-49ac-93f4-91a400113771&at=AENtkXZvDDLLgz_4fIeISizPJm6T:1731087694473' \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'accept-language: zh-CN,zh;q=0.9,ja;q=0.8' \
+  -H 'cookie: HSID=AoR-WmzWLXBAHQqKo; SSID=ASCak90N7kozCW1aj; APISID=FeOy93caN786u_Fp/A9s57PXdSwAy2q_Vr; SAPISID=ZV6S5SbV9xufsCMc/AFeFBSHEWGcfLuhKC; __Secure-1PAPISID=ZV6S5SbV9xufsCMc/AFeFBSHEWGcfLuhKC; __Secure-3PAPISID=ZV6S5SbV9xufsCMc/AFeFBSHEWGcfLuhKC; SID=g.a000owjLvoFPd4uoL3d31v9M9bONWAekICbjYuValv4qHzJqCQfvtvGox5ldeJtTZjy7hiHdSQACgYKAfcSARYSFQHGX2MiEUZvXdSemzdU7QhNDw0l6BoVAUF8yKoadIgPvNHr0VWUv78aG5bZ0076; __Secure-1PSID=g.a000owjLvoFPd4uoL3d31v9M9bONWAekICbjYuValv4qHzJqCQfvmXt3VJ0AG-wubEh3jNbp_QACgYKARoSARYSFQHGX2Mirs-wG26RYcIRHaulogLrWhoVAUF8yKrUl5aQSa3OWIjx5oFj-sOr0076; __Secure-3PSID=g.a000owjLvoFPd4uoL3d31v9M9bONWAekICbjYuValv4qHzJqCQfvKocG7prnpgLHbHCcj7nSgwACgYKAQkSARYSFQHGX2MiLx1f_pAtjEUla7ntcZI-whoVAUF8yKr_qGGoGEzVohJZYjFa4Nnu0076; SEARCH_SAMESITE=CgQIw5wB; AEC=AVYB7cqHrS1DBV9nR6XSbN9qDyYZWkjRiBpC8GZuQCtJM1S4Rnj4w66yGg; NID=519=AJQz7cVdBOkb3kQFHOHHWNwx0769ogDPnG59jvjjSTxFn8yEMGwl1JwelRtDkfJWXgwa3rE0ithJqrgQzd7yYSmQkgBSX_wMl508MOlkEuYKdZRATUVj_1_9CJmKhAyY1cT9WnAj8CKkALaz1D2Z2KQkgov0bux6YgAok8G_T3cOcOL4ozt-EA3WA4uD9TAUHc-KGSCCaeqt8_igd-7R4ornuNoqlpHPMCLbz2mayNi3a_NcL_3mAvDrOEsA2RLh3at0OGCmG4BxvF3qXQxgYlW7b7PTOM5pTgc65RVUUMgRHcqLr7LYz-vXEGxHMaIO1GkaToiShiQsHBk2cNR5yt0j8nKnZHuudplRqDqqGUhMi0mVsL5cq3iP0Fu48TWINUqAKkBOkaICzVzjcud-THhVPp1qMg-bLzD543CZZmIK282mrMePUKiwlD9oeKNvtbG94ZY8J-MEr-EVq9GtWN622qAyaKbfByIOpoaGZ9lYdzTgWnrqcWGs_YoiPwB8cJzDX413-PPPhe-9xKZvxOZ1rBA9oJj0laww2Dhst6vbaq0e4jpL0g73ioYNLuXPpwuXOa6idG8lnaeRfjGMV1eJUg; __Secure-1PSIDTS=sidts-CjEBQT4rXwBEfKezxL0WehwMJXAx5roStMBHwRB9Luf_Htac9hgvfcZheMWleQL6BeFOEAA; __Secure-3PSIDTS=sidts-CjEBQT4rXwBEfKezxL0WehwMJXAx5roStMBHwRB9Luf_Htac9hgvfcZheMWleQL6BeFOEAA; SIDCC=AKEyXzWPPq2FiaCmDvSZRpEJQeCj5Lds1G8w_NCcajDhdm9OO5Tc-725HHxc6DD4UbcTSukc-eQ; __Secure-1PSIDCC=AKEyXzVOjA8uAzNZrVOZQb_aqsOcApYA2NnY8-_JkzPTjNx9vZG63nE-moVcAfVxFsC220WFOw; __Secure-3PSIDCC=AKEyXzUyONlYWUFtGaBfPp7tjTbmUjKCtldQXES57KB7oasY5nteZcVe4dmO5Qj-uS5h-X5JV-Q' \
+  -H 'priority: u=0, i' \
+  -H 'sec-ch-ua: "Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"' \
+  -H 'sec-fetch-dest: iframe' \
+  -H 'sec-fetch-mode: navigate' \
+  -H 'sec-fetch-site: same-site' \
+  -H 'upgrade-insecure-requests: 1' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36' \
+  -H 'x-browser-channel: stable' \
+  -H 'x-browser-copyright: Copyright 2024 Google LLC. All rights reserved.' \
+  -H 'x-browser-validation: QTcFh+5ZedCCidQkKanqiUwPHQo=' \
+  -H 'x-browser-year: 2024' \
+  -H 'x-client-data: CJG2yQEIpLbJAQipncoBCPyEywEIlqHLAQic/swBCICZzQEIhaDNAQj+pc4BCPu+zgEI+cnOAQiRys4BCJjLzgEIw8zOAQjFzM4BCNLNzgEIxs/OAQj1z84BGJyxzgEYgMrOARiUzc4B' -o checkpoints.zip
+unzip checkpoints.zip
